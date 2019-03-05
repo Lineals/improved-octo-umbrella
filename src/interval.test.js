c0527affcfc89a17a1807cd8d89fcb8464c39e1b
Lineal <linealsp@gmail.com>
2019-03-05 15:35:58 +0100

    add interval tests

---
+++ src/interval.test.js
@@ -0,0 +1,17 @@
+const Interval = require('./interval')
+
+describe('Includes', function () {
+    test.each([
+        [3, 10, 4, 9, true],
+        [5, 10, 4, 9, false],
+        [1, 2, 4, 9, false],
+        [1, 20, 0, 9, false],
+        [10, 20, 1, 2, false]
+    ])(
+
+        'Interval (%i,%i) is around (%i,%i) : %p',
+        (a1,a2,b1,b2, expected) => {
+            expect(new Interval(a1,a2).includes(new Interval(b1,b2))).toBe(expected);
+        },
+    );
+});
