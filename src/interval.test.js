e4ed5c0ac4b7fa7124005e388828fbb159477a0e
Lineal <linealsp@gmail.com>
2019-03-05 15:48:36 +0100

    Overlaps tests

--- src/interval.test.js
+++ src/interval.test.js
@@ -1,5 +1,21 @@
 const Interval = require('./interval')
 
+describe('Overlaps', function () {
+    test.each([
+        [3, 10, 4, 9, true],
+        [5, 10, 4, 9, true],
+        [1, 2, 4, 9, false],
+        [1, 20, 0, 9, true],
+        [10, 20, 1, 2, false]
+    ])(
+
+        'Interval (%i,%i) overlaps (%i,%i) : %p',
+        (a1,a2,b1,b2, expected) => {
+            expect(new Interval(a1,a2).overlaps(new Interval(b1,b2))).toBe(expected);
+        },
+    );
+});
+
 describe('Includes', function () {
     test.each([
         [3, 10, 4, 9, true],
