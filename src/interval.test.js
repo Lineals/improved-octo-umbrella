4c500f553159236951f1ffdbae07358b685454e1
Lineal <linealsp@gmail.com>
2019-03-05 16:14:04 +0100

    Tests unions

--- src/interval.test.js
+++ src/interval.test.js
@@ -31,3 +31,20 @@ describe('Includes', function () {
         },
     );
 });
+
+describe('Union', function () {
+    test.each([
+        [3, 10, 4, 9, [new Interval(3,10)]],
+        [5, 10, 4, 9, [new Interval(4,10)]],
+        [1, 2, 4, 9, [new Interval(1,2), new Interval(4,9)]],
+        [1, 20, 0, 9, [new Interval(0,20)]],
+        [10, 20, 1, 2, [new Interval(1,2), new Interval(10, 20)]]
+    ])(
+
+        'Interval (%i,%i) is around (%i,%i) : %p',
+        (a1,a2,b1,b2, expected) => {
+            expect(new Interval(a1,a2).union(new Interval(b1,b2))).toEqual(expected);
+        },
+    );
+});
+
