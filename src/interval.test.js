f73002204e2b63f4118d3ca36a637b7b271676e9
Lineal <linealsp@gmail.com>
2019-03-12 13:43:39 +0100

    add test case

--- src/interval.test.js
+++ src/interval.test.js
@@ -34,14 +34,17 @@ describe('Includes', function () {
 
 describe('Union', function () {
     test.each([
-        [3, 10, 4, 9, [new Interval(3,10)]],
+        [1, 2, 1, 2, [new Interval(1,2)]],
+        [0, 1, 1, 2, [new Interval(0,2)]],
+        [1, 2, 0, 1, [new Interval(0,2)]],
+        [3, 10, 5, 10, [new Interval(3,10)]],
         [5, 10, 4, 9, [new Interval(4,10)]],
         [1, 2, 4, 9, [new Interval(1,2), new Interval(4,9)]],
         [1, 20, 0, 9, [new Interval(0,20)]],
         [10, 20, 1, 2, [new Interval(1,2), new Interval(10, 20)]]
     ])(
 
-        'Interval (%i,%i) is around (%i,%i) : %p',
+        'Interval (%i,%i) (%i,%i)',
         (a1,a2,b1,b2, expected) => {
             expected.forEach(function(element){
                 expect(new Interval(a1,a2).union(new Interval(b1,b2))).toContainEqual(element);
